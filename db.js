a788a4f7946bc97ccc850e30e365e8376df3e2f1
kush aggarwal <kush.aggarwal110085@gmail.com>
2020-05-24 17:32:13 +0530

    Changes resolved

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
 const mysql = require("mysql2");
 
-// const connection  = mysql.createConnection({
-// host: 'localhost',
-// database: 'mytestdb2',
-// user: 'myuser2',
-// password: 'mypass2'
-// })
-const connection = mysql.createConnection(
-  "postgres://jcuvtyqplhxxdu:b0bd587b74ec20c1c95b78b92d0b0f2ebe0a5bcd76c267d469496ee4b606cced@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d32iu12fg6gggj"
-);
+const connection = mysql.createConnection({
+  host: "localhost",
+  database: "mytestdb2",
+  user: "myuser2",
+  password: "mypass2",
+});
 
 function getAllPersons() {
   return new Promise(function (resolve, reject) {
