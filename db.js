724fdb40bbe723eea8d3ae7aaa18219b85cf6fb4
kush aggarwal <kush.aggarwal110085@gmail.com>
2020-05-24 17:27:26 +0530

    DB configured

--- db.js
+++ db.js
@@ -1,49 +1,43 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
- 
+// const connection  = mysql.createConnection({
+// host: 'localhost',
+// database: 'mytestdb2',
+// user: 'myuser2',
+// password: 'mypass2'
+// })
+const connection = mysql.createConnection(
+  "postgres://jcuvtyqplhxxdu:b0bd587b74ec20c1c95b78b92d0b0f2ebe0a5bcd76c267d469496ee4b606cced@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d32iu12fg6gggj"
+);
 
-const connection  = mysql.createConnection({
-host: 'localhost',
-database: 'mytestdb2',
-user: 'myuser2',
-password: 'mypass2'
-})
-
-
-function getAllPersons(){
-
-    return new Promise(function(resolve,reject){
-     connection.query(
-         `SELECT * FROM persons`,
-         function(err,rows,cols){
-             if(err)
-             reject(err)
-             else {
-                 resolve(rows)
-             }
-         })
-    })
+function getAllPersons() {
+  return new Promise(function (resolve, reject) {
+    connection.query(`SELECT * FROM persons`, function (err, rows, cols) {
+      if (err) reject(err);
+      else {
+        resolve(rows);
+      }
+    });
+  });
 }
 
-function addNewPerson(name,ate,thick,material,time,job){
-    return new Promise (function(resolve,reject){
-        connection.query(
-            `INSERT INTO persons (name,ate,thick,material,time,job) VALUES (?,?,?,?,?,?)`,
-            [name,ate,thick,material,time,job],
-            function(err, results){
-                if(err){
-                    reject(err)
-                } else { 
-                    resolve()
-                }
-
-            }
-        )
-    })
+function addNewPerson(name, ate, thick, material, time, job) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `INSERT INTO persons (name,ate,thick,material,time,job) VALUES (?,?,?,?,?,?)`,
+      [name, ate, thick, material, time, job],
+      function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
 }
-
 
 exports = module.exports = {
-    getAllPersons,
-    addNewPerson
-}
+  getAllPersons,
+  addNewPerson,
+};
--- server.js
+++ server.js
@@ -1,80 +1,83 @@
-const express = require('express')
-const app = express()
-const db = require('./db')
-const bodyParser = require('body-parser')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const nodemailer = require('nodemailer')
-const PORT = process.env.PORT || 3987
+const express = require("express");
+const app = express();
+const db = require("./db");
+const bodyParser = require("body-parser");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const PORT = process.env.PORT || 3987;
 
-app.engine('handlebars', exphbs())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.engine("handlebars", exphbs());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //this is used to render the html file saved in the views folder
-app.set("view engine", "hbs")
-app.use(express.static('public'));
-app.get('/job',(req,res)=>{
-
-    db.getAllPersons()  
-       .then((persons) => {
-        res.render('job', {persons})
-         })
-       .catch((err)=>{
-             res.send(err) })
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+app.get("/job", (req, res) => {
+  db.getAllPersons()
+    .then(persons => {
+      res.render("job", { persons });
     })
-
-    app.get('/about',(req,res)=>{
-        res.render('about')
-    })
-
-app.get('/contact',(req,res)=>{
-    res.render('contact')
-})
-
-app.get('/events',(req,res)=>{
-    res.render('Events')
-})
-
-app.get('/main',(req,res)=>{
-    res.render('main')
-})
-app.get('/gallery',(req,res)=>{
-    res.render('gallery')
-})
-app.get('/new',(req,res)=>{
-    res.render('new')
-})
-app.get('/add',(req,res)=>{
-    res.render('job_add')
-})
-
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-app.post('/add',(req,res)=>{
-     db.addNewPerson(req.body.name,req.body.ate,req.body.thick,req.body.material,
-        req.body.time,req.body.job )
-        .then(()=>{
-            res.redirect('/job')
-        })
-        .catch((err)=>{
-            res.send(err)
-        })
-})
-app.listen(PORT, ()=>{
-    console.log("Server started at localhost 4444")
-})
-
-
-
-
-app.use('/public', express.static(path.join(__dirname,'public')))
-app.get('/', (req,res) => {
-res.render('contact')
+    .catch(err => {
+      res.send(err);
+    });
 });
 
-app.post('/send',(req,res)=>{
-    const output = `
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/events", (req, res) => {
+  res.render("Events");
+});
+
+app.get("/main", (req, res) => {
+  res.render("main");
+});
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+app.get("/add", (req, res) => {
+  res.render("job_add");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/add", (req, res) => {
+  db.addNewPerson(
+    req.body.name,
+    req.body.ate,
+    req.body.thick,
+    req.body.material,
+    req.body.time,
+    req.body.job
+  )
+    .then(() => {
+      res.redirect("/job");
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+app.listen(PORT, () => {
+  console.log("Server started at localhost 4444");
+});
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/send", (req, res) => {
+  const output = `
     <p>You have a new contact request</p>
     <h3>Contact Details</h3>
     <ul>
@@ -83,50 +86,44 @@ app.post('/send',(req,res)=>{
     <li>Message: ${req.body.message}</li>
     <li>Number: ${req.body.number}</li>
     </ul>`;
-    nodemailer.createTestAccount((err, account) => {
-        if (err) {
-            console.error('Failed to create a testing account. ' + err.message);
-            return process.exit(1);
-        }
-    
-        console.log('Credentials obtained, sending message...');
-    
-        // Create a SMTP transporter object
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            auth: {
-                user: 'jiiyyv5hj6wez47u@ethereal.email',
-                pass: 'nvaXDVafRWqMat2TdV'
-            }
-        });
-    
-        // Message object
-        let message = {
-            from: 'Sender Name <kush.aggarwal110085@gmail.com>',
-            to: 'Recipient <hmwlaser@gmail.com>',
-            subject: 'Nodemailer is unicode friendly ✔',
-            text: 'Hello to myself!',
-            html: output
-        };
-    
-        transporter.sendMail(message, (err, info) => {
-            if (err) {
-                console.log('Error occurred. ' + err.message);
-                return process.exit(1);
-            }
-    
-            console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  nodemailer.createTestAccount((err, account) => {
+    if (err) {
+      console.error("Failed to create a testing account. " + err.message);
+      return process.exit(1);
+    }
 
-            res.render('contact',{msg:'Email has been sent'})
-        });
+    console.log("Credentials obtained, sending message...");
+
+    // Create a SMTP transporter object
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "jiiyyv5hj6wez47u@ethereal.email",
+        pass: "nvaXDVafRWqMat2TdV"
+      }
     });
-    
-})
 
+    // Message object
+    let message = {
+      from: "Sender Name <kush.aggarwal110085@gmail.com>",
+      to: "Recipient <hmwlaser@gmail.com>",
+      subject: "Nodemailer is unicode friendly ✔",
+      text: "Hello to myself!",
+      html: output
+    };
 
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.log("Error occurred. " + err.message);
+        return process.exit(1);
+      }
 
+      console.log("Message sent: %s", info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-
+      res.render("contact", { msg: "Email has been sent" });
+    });
+  });
+});
